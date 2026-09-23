3d1fd399f0849717368a2371f39f6c28a6ce14a3
EastMonster <east_monster@126.com>
2026-09-23 23:57:48 +0800

    feat: datafix v5

---
+++ src/main/java/monster/east/matchaff/datafix/schema/V5.java
@@ -0,0 +1,9 @@
+package monster.east.matchaff.datafix.schema;
+
+import com.mojang.datafixers.schemas.Schema;
+
+public class V5 extends V4 {
+	public V5(int versionKey, Schema parent) {
+		super(versionKey, parent);
+	}
+}
